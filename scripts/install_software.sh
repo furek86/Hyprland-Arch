05b9a1272909c5bf4bccdfa053e26ba6e5051013
Jakub Fuczek <furek86@gmail.com>
2025-08-06 09:50:24 +0200

    new font and style for ghostty

---
+++ scripts/install_software.sh
@@ -0,0 +1,19 @@
+# First we install the required packages
+sudo pacman -Syu
+
+sudo pacman -S --noconfirm \
+    git \
+    base-devel \
+    wget \
+    curl \
+    neovim \
+    starship \
+    zsh \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting \
+    ripgrep \
+    fd \
+    bat \
+    exa \
+    fzf \
+    
